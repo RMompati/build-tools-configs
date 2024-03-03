f21c921753dbe73ac682ff4def502fe1871fc28d
Mompati Erold Keetile <43243167+RMompati@users.noreply.github.com>
2024-03-03 20:53:12 +0200

    Update init-git

--- init-git.sh
+++ init-git.sh
@@ -1,17 +1,13 @@
 #! /bin/bash
 
-git alias.pull pl
-git alias.pull pl
-git alias.pull pl
-git alias.pull pl
-git alias.pull pl
-git alias.pull pl
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.cim "commit -m"
+git config --global alias.br branch
+git config --global alias.bra "branch -a"
+git config --global alias.pl pull
+git config --global alias.ps push
+git config --global alias.cl "config --list"
+git config --global alias.ri "rebase -i"
 
-alias.st=status
-alias.co=checkout
-alias.ci=commit
-alias.cim=commit -m
-alias.br=branch
-alias.bra=branch -a
-alias.pl=pull
-alias.ps=push
