7d91138740c52552c44d6e671da9434102ca6605
Mompati Erold Keetile <43243167+RMompati@users.noreply.github.com>
2024-03-03 20:46:32 +0200

    Create init-git.sh

---
+++ init-git.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+git alias.pull pl
+git alias.pull pl
+git alias.pull pl
+git alias.pull pl
+git alias.pull pl
+git alias.pull pl
+
+alias.st=status
+alias.co=checkout
+alias.ci=commit
+alias.cim=commit -m
+alias.br=branch
+alias.bra=branch -a
+alias.pl=pull
+alias.ps=push
